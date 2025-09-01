8187e754e902b9f45b4b857035108b6d2e1e1af5
Pedro Henrique <pedro.romanowski02@gmail.com>
2025-08-31 23:00:06 -0300

    Mudando nome e adicionando metodo de buscar cliente pelo email

--- src/main/java/com/pedrohenrique/bibliotecavirtual/adapter/output/repository/ClienteRepository.java
+++ src/main/java/com/pedrohenrique/bibliotecavirtual/adapter/output/repository/ClienteRepository.java
@@ -1,7 +1,12 @@
 package com.pedrohenrique.bibliotecavirtual.adapter.output.repository;
 
+import com.pedrohenrique.bibliotecavirtual.adapter.output.entity.ClienteEntity;
 import com.pedrohenrique.bibliotecavirtual.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClienteRepository extends JpaRepository<Cliente ,Long> {
+
+    Optional<ClienteEntity> findByEmailIgnoreCase(String email);
 }
