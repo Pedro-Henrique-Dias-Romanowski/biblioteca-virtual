f276af67a51fac3eaa8d13fe54798022c7fcd876
Pedro Henrique <pedro.romanowski02@gmail.com>
2025-08-31 23:00:49 -0300

    modificando metodos

--- src/main/java/com/pedrohenrique/bibliotecavirtual/domain/port/output/ClienteOuputPort.java
+++ src/main/java/com/pedrohenrique/bibliotecavirtual/domain/port/output/ClienteOuputPort.java
@@ -2,14 +2,12 @@ package com.pedrohenrique.bibliotecavirtual.domain.port.output;
 
 import com.pedrohenrique.bibliotecavirtual.domain.entity.Cliente;
 import com.pedrohenrique.bibliotecavirtual.domain.entity.Emprestimo;
-import com.pedrohenrique.bibliotecavirtual.domain.entity.Livro;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface ClienteOuputPort {
 
-    List<Livro> visualizarTodosOsLivros();
+    List<Emprestimo> visualizarTodosOsEmprestimos();
     Optional<Emprestimo> emprestarLivro(Emprestimo emprestimo);
     Cliente cadastrarCliente(Cliente cliente);
     void efetuarLogin();
